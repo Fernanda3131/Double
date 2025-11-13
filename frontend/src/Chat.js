dc62837c59715dadd0afc5bd29b2949e5e2b9b67
JuanArcila1 <arcilacalos@gmail.com>
2025-11-13 11:46:55 -0500

    Cambios nuevos

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -333,7 +333,7 @@ function Chat({ id_destinatario, destinatarioInfo, onBack, mensajeInicial }) {
             flexShrink: 0,
           }}
         >
-          ← Mensajes
+          ← mensa
         </button>
       )}
 
